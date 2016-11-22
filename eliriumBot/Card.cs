314ddfe9144285841c6579e5b08a5caac414a199
Leonid Burdikov <leonid.b.d@gmail.com>
2016-11-22 03:24:40 +0300

    initial commit

---
+++ eliriumBot/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eliriumBot
+{
+    class Card
+    {
+        public int DefaultCount { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+
+        public Card(string name, int defCount, string desc)
+        {
+            Name = name;
+            DefaultCount = defCount;
+            Description = desc;
+        }
+    }
+}
---
+++ eliriumBot/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace eliriumBot
+{
+    class Program
+    {
+        private const string token = "289179199:AAFbRCxuvIoX_Cg0TpowgDLc5dlnLYuxaPA";
+
+        private static TelegramBotClient bot;
+
+        private static Dictionary<long, GameRoom> rooms = new Dictionary<long, GameRoom>();
+
+        static void Main(string[] args)
+        {
+            bot = new TelegramBotClient(token);
+            bot.OnMessage += Bot_OnMessage;
+            bot.StartReceiving();
+
+            while (true) { }
+        }
+
+        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs args)
+        {
+            var id = args.Message.Chat.Id;
+            if (!rooms.ContainsKey(id))
+            {
+                GameRoom room = new GameRoom(id, bot);
+                rooms.Add(id, room);
+            }
+            rooms[id].HandleMessage(args);
+        }
+    }
+}
